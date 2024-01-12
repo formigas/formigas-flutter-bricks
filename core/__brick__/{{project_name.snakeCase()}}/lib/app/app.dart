89ae50f993c58e9834b24c51a0d0938591a46854
Viktor <viktor@formigas.de>
2024-01-12 15:36:35 +0100

    dependency sorting updated

--- core/__brick__/{{project_name.snakeCase()}}/lib/app/app.dart
+++ core/__brick__/{{project_name.snakeCase()}}/lib/app/app.dart
@@ -1,8 +1,8 @@
 import 'package:{{project_name.snakeCase()}}/common/custom_themes.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:{{project_name.snakeCase()}}/feature/counter/counter_controller_implementation.dart';
 import 'package:{{project_name.snakeCase()}}/feature/counter/counter_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
