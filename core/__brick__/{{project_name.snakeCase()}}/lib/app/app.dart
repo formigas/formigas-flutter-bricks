c2cd623d4eac79adc0f2a0723d46b7607ed17777
Viktor <viktor@formigas.de>
2024-02-08 15:45:59 +0100

    added go_router template

--- core/__brick__/{{project_name.snakeCase()}}/lib/app/app.dart
+++ core/__brick__/{{project_name.snakeCase()}}/lib/app/app.dart
@@ -2,17 +2,20 @@ import 'package:{{project_name.snakeCase()}}/common/custom_themes.dart';
 import 'package:{{project_name.snakeCase()}}/feature/counter/counter_controller.dart';
 import 'package:{{project_name.snakeCase()}}/feature/counter/counter_view.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+part 'router.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: _appRouter,
       title: '{{project_name.pascalCase()}} App',
       theme: CustomTheme().light,
       darkTheme: CustomTheme().dark,
-      home: CounterView(controller: CounterControllerImplementation()),
     );
   }
 }
