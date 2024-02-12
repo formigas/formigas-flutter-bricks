f9aac6c66a08988bfe25bd91e746748440e92763
Viktor <viktor@formigas.de>
2024-02-12 16:40:14 +0100

    added flutter_gen package with optional svg integration

--- core/__brick__/{{project_name.snakeCase()}}/test/helpers/pump_app.dart
+++ core/__brick__/{{project_name.snakeCase()}}/test/helpers/pump_app.dart
@@ -1,4 +1,4 @@
-import 'package:{{project_name.snakeCase()}}/l10n/l10n.dart';
+import 'package:{{project_name.snakeCase()}}/common/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
