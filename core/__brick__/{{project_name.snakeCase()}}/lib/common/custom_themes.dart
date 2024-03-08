9b069e8979e6d108c66b8dcf0833764b70ffb20a
Lea Keil <lea.keil@formigas.de>
2024-03-08 16:09:06 +0100

    feature: optimising theme

--- core/__brick__/{{project_name.snakeCase()}}/lib/common/custom_themes.dart
+++ core/__brick__/{{project_name.snakeCase()}}/lib/common/custom_themes.dart
@@ -7,13 +7,13 @@ class CustomTheme {
         useMaterial3: {{#use_material_3}}true{{/use_material_3}}{{^use_material_3}}false{{/use_material_3}},
         brightness: Brightness.dark,
         colorScheme: darkColorScheme,
-        textTheme: textTheme,
+        textTheme: textTheme(darkColorScheme),
       );
 
   ThemeData get light => ThemeData(
         useMaterial3: {{#use_material_3}}true{{/use_material_3}}{{^use_material_3}}false{{/use_material_3}},
         brightness: Brightness.light,
         colorScheme: lightColorScheme,
-        textTheme: textTheme,
+        textTheme: textTheme(lightColorScheme),
       );
 }
