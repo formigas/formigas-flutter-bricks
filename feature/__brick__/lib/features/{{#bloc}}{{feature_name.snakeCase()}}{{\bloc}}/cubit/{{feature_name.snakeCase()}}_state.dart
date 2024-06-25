b6abf3702cf632faae4281c8021db876b9566ba3
SeeBehr <131759379+SeeBehr@users.noreply.github.com>
2024-06-25 16:44:06 +0200

    Feature/add bloc as state management solution (#11)
    * fix: correct terminal commands
    
    * fix: correct repository url
    
    * ref: update ssh/https description
    
    * feat: add new initialisation prompt
    
    * feat: add BLoC feature implementation
    
    * fix: newlines around state class
    
    * ref: make bloc view a stateless widget
    
    * fix: correct copyWith to have optional named parameter
    
    * ref: rephrase state management question and variable name
    
    * ref: remove riverpod methods and rename library to package
    
    * ref: remove pre_gen riverpod case
    
    * ref: update state management description
    
    * feat: add error handling for incomplete pubspec
    
    * feat: add  tests for bloc
    
    * ref: move constructor, to fit linter rules
    
    * ref: apply linter rules
    
    * fix: remove final to apply formigas_core linter rules
    
    * ref: move cubit folder into feature folder
    
    * fix: correct path to freezed part
    
    * chore: remove whitespace
    
    * feat: add tests for cubit
    
    * ref: rename test file
    
    * fix: add bloc_test as dependency

---
+++ feature/__brick__/lib/features/{{#bloc}}{{feature_name.snakeCase()}}{{\bloc}}/cubit/{{feature_name.snakeCase()}}_state.dart
@@ -0,0 +1,19 @@
+{{#use_freezed}}import 'package:freezed_annotation/freezed_annotation.dart';
+
+part '{{feature_name.snakeCase()}}_state.freezed.dart';
+
+@freezed
+class {{feature_name.pascalCase()}}State with _${{feature_name.pascalCase()}}State {
+  const factory {{feature_name.pascalCase()}}State({required int count}) = _{{feature_name.pascalCase()}}State;
+
+  factory {{feature_name.pascalCase()}}State.initial() => const {{feature_name.pascalCase()}}State(count: 0);
+}{{/use_freezed}}{{^use_freezed}}class {{feature_name.pascalCase()}}State {
+  final int _count;
+  int get count => _count;
+
+  const {{feature_name.pascalCase()}}State({required final int count}): _count = count;
+
+  factory {{feature_name.pascalCase()}}State.initial() => const {{feature_name.pascalCase()}}State(count: 0);
+
+  {{feature_name.pascalCase()}}State copyWith({final int? count}) => {{feature_name.pascalCase()}}State(count: count ?? _count);
+}{{/use_freezed}}
---
+++ feature/__brick__/lib/features/{{#bloc}}{{feature_name.snakeCase()}}{{\bloc}}/{{feature_name.snakeCase()}}_view.dart
@@ -0,0 +1,37 @@
+import 'package:{{project_name.snakeCase()}}/features/{{feature_name.snakeCase()}}/cubit/{{feature_name.snakeCase()}}_cubit.dart';
+import 'package:{{project_name.snakeCase()}}/features/{{feature_name.snakeCase()}}/cubit/{{feature_name.snakeCase()}}_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class {{feature_name.pascalCase()}}View extends StatelessWidget {
+  const {{feature_name.pascalCase()}}View({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Flutter Demo Home Page'),
+        ),
+        body: BlocBuilder<{{feature_name.pascalCase()}}Cubit, {{feature_name.pascalCase()}}State>(
+          builder: (context, state) => Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '${state.count}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: context.read<{{feature_name.pascalCase()}}Cubit>().increment,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
+      );
+}
